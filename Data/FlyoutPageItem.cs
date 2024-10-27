0dfda2793f3c310b22ced68119d03dfa0d04badb
jose gilberto caceres madrid <caceresmadrid@hotmail.com>
2024-10-27 14:10:56 -0600

    Repositorio Inicial subir a git

---
+++ Data/FlyoutPageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoMAUI.Data
+{
+    internal class FlyoutPageItem
+    {
+        public string Titulo { get; set; }
+        public string RutaImagen { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
---
+++ Pages/Pagina1.xaml.cs
@@ -0,0 +1,9 @@
+namespace CursoMAUI.Pages;
+
+public partial class Pagina1 : ContentPage
+{
+	public Pagina1()
+	{
+		InitializeComponent();
+	}
+}
